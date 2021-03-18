53737917f02adf2ef3714d672c220fdb60f5941f
SOFe <sofe2038@gmail.com>
2021-03-18 17:14:29 +0800

    Show timestamps

--- reader/src/reader.py
+++ reader/src/reader.py
@@ -2,9 +2,10 @@
 
 import rospy
 from std_msgs.msg import String
+import time
 
 def handler(msg):
-    print("Received message: {}".format(msg))
+    rospy.loginfo("Received message at {}: {}".format(time.ctime(), msg))
 
 rospy.init_node("reader", xmlrpc_port=22920, tcpros_port=22921)
 pub = rospy.Subscriber("/talk", String, handler)
--- talker/src/talker.py
+++ talker/src/talker.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import rospy
+import time
 from std_msgs.msg import String
 
 rospy.init_node("talker", xmlrpc_port=22920, tcpros_port=22921)
 pub = rospy.Publisher("/talk", String)
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
-    pub.publish("Hello")
+    pub.publish("Hello at {}".format(time.ctime()))
     rate.sleep()
